fb2e00fe120925cd4451187678b2efee3d5c80d7
Savantah <stherclair@gmail.com>
2024-10-05 14:58:43 -0400

    Initial Commit

---
+++ Memory games/Memory_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_gamesApp.swift
+//  Memory games
+//
+//  Created by Star Lina on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory gamesTests/Memory_gamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_gamesTests.swift
+//  Memory gamesTests
+//
+//  Created by Star Lina on 10/5/24.
+//
+
+import Testing
+@testable import Memory_games
+
+struct Memory_gamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
